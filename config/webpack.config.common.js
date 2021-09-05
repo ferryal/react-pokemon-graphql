5dccad29c8157dd4729b3904eebc2f3ce6614436
ferryal <ferryal21@gmail.com>
2021-09-05 12:37:42 +0700

    feat: config webpack dev & prod

---
+++ config/webpack.config.common.js
@@ -0,0 +1,62 @@
+// @ts-check
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function _a(...args) {
+  return args;
+}
+
+/** @type {import('webpack').Configuration} */
+const commonConfig = {
+  entry: {
+    app: ['react-hot-loader/patch', './src/index.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    filename: '[name].[contenthash].bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              _a('@babel/preset-react', { runtime: 'automatic' }),
+            ],
+            plugins: ['react-hot-loader/babel'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|png|gif|svg|pdf|ico|mp3)$/,
+        loader: require.resolve('file-loader'),
+        options: {
+          limit: 10000,
+          name: 'assets/[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      minify: false,
+      template: 'public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
+module.exports = commonConfig;
