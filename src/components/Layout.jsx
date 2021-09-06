c0b36793dcfe365be9f8ed7f8c971f9b68c8a364
ferryal <ferryal21@gmail.com>
2021-09-06 18:19:57 +0700

    feat: add component layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 6rem;
+`;
+
+const Layout = (props) => {
+  const {
+    rounded, elevated, children, style, className, ...restProps
+  } = props;
+  return (
+    <>
+      <Wrapper
+        {...restProps}
+      >
+        {children}
+      </Wrapper>
+    </>
+  );
+};
+
+export default Layout;
